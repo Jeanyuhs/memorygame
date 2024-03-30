ab9b39b502f7a2a8fafcae09f0f6f646a1682004
Jeanyuhs <87541595+Jeanyuhs@users.noreply.github.com>
2024-03-30 18:44:56 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 document.addEventListener('DOMContentLoaded', () => {
 
-    //card options
+    // card options
     const cardArray = [
-                {
+        {
             name: 'blackKoi',
             img: 'images/blackKoi.png'
         },
@@ -54,14 +54,13 @@ document.addEventListener('DOMContentLoaded', () => {
 
     cardArray.sort(() => 0.5 - Math.random())
 
-
     const grid = document.querySelector('.grid')
     const resultDisplay = document.querySelector('#result')
     var cardsChosen = []
     var cardsChosenId = []
     var cardsWon = []
 
-    //create game board
+    // create game board
     function createBoard() {
         for (let i = 0; i < cardArray.length; i++) {
             var card = document.createElement('img')
@@ -72,67 +71,44 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     }
 
-    const imgBlank = document.querySelector('.img-blank')
-
-    
     // check for matches
     function checkForMatch() {
         var cards = document.querySelectorAll('img')
         const optionOneId = cardsChosenId[0]
         const optionTwoId = cardsChosenId[1]
 
-        if ((cardsChosen[0] === cardsChosen[1]) && (optionOneId !== optionTwoId)) { // with out && players would be able to select same image twice to get a match
-            // alert('You found a match') - removed due to having to click to remove prompt to enhance user experience
-            
+        if ((cardsChosen[0] === cardsChosen[1]) && (optionOneId !== optionTwoId)) {
             cards[optionOneId].setAttribute('src', 'images/blank.png')
             cards[optionTwoId].setAttribute('src', 'images/blank.png')
-            
-            
+            cards[optionOneId].removeEventListener('click', flipCard)
+            cards[optionTwoId].removeEventListener('click', flipCard)
             cardsWon.push(cardsChosen)
-        } 
-        else {
+        } else {
             cards[optionOneId].setAttribute('src', 'images/back.png')
             cards[optionTwoId].setAttribute('src', 'images/back.png')
-            // alert('Sorry, try again') - removed due to having to click to remove prompt to enhance user experience
-        }  
+        }
 
-        
         cardsChosen = []
         cardsChosenId = []
         resultDisplay.textContent = cardsWon.length
         
         if (cardsWon.length === cardArray.length/2) {
             resultDisplay.textContent = 'Congratulations! You found them all!'
-         }
-   
-
+        }
     }
 
-
     // flip card
-    // added eventListener to prevent more than two cards being clicked at a time
-    document.addEventListener("click", e => {
-        if (cardsChosen.length === 2) {
-            e.stopPropagation();
-            e.preventDefault();
-        } 
-    }, true);
-
-    
     function flipCard() {
-
         var cardId = this.getAttribute('data-id')
-        cardsChosen.push(cardArray[cardId].name)
-        cardsChosenId.push(cardId)
-        this.setAttribute('src', cardArray[cardId].img)
-         
-        if (cardsChosen.length === 2) {
-            setTimeout(checkForMatch, 500)
+        if (!cardsWon.flat().includes(cardArray[cardId].name)) {
+            cardsChosen.push(cardArray[cardId].name)
+            cardsChosenId.push(cardId)
+            this.setAttribute('src', cardArray[cardId].img)
+            if (cardsChosen.length === 2) {
+                setTimeout(checkForMatch, 500)
+            }
         }
-
     } 
 
-createBoard()
-
-
+    createBoard()
 })
